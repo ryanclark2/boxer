043290bd8c2bd9b8dcf006c3afcc872b56c88c24
Ryan Clark <rclark.sf@gmail.com>
2012-08-06 16:36:07 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boxer::Application.config.secret_token = '789e2c2f14f3c071d533902d70877553afc2930db8d100ddbe94634ef9854c6921804cf50948e487fa9ddf5366d54f76c705827daafa4302adb7feaee9e0ab8e'
